6670d08c9bb3d3152d8c28aec188cf893595c9ad
nainghtweaung <114207394+nainghtweaung@users.noreply.github.com>
2024-02-20 20:37:04 +0630

    Add function to check winner

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ const gameBoard = (function () {
     }
   };
 
-  const drawMarker = (row, column, player) => {
+  const dropMarker = (row, column, player) => {
     board[row][column] = GridCell().addMarker(player);
   };
 
-  return {getBoard, initializeGameBoard, drawMarker, cellIsAvailable};
+  return {getBoard, initializeGameBoard, dropMarker, cellIsAvailable};
 })();
 
 function GridCell() {
@@ -51,43 +51,102 @@ const gameController = (function () {
   gameBoard.initializeGameBoard();
   const board = gameBoard.getBoard();
 
-  let currentPlayer = "player1";
+  const players = [
+    {
+      id: "player1",
+      token: 1,
+    },
+    {
+      id: "player2",
+      token: 2,
+    },
+  ];
+
+  let currentPlayer = players[0];
+  let winner = null;
+
+  const getCurrentPlayer = () => currentPlayer;
+  const getWinner = () => winner;
 
   const switchPlayer = () => {
-    currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+    currentPlayer = currentPlayer.id === "player1" ? players[1] : players[0];
   };
 
-  const checkWinner = () => {
-    const cells = [];
-    let index = 0;
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[0].length; j++) {
-        cells[index] = board[i][j];
-        index++;
-      }
-    }
+  const checkWinner = (player) => {
+    const cells = [...board[0], ...board[1], ...board[2]];
     console.log("Cell array", cells);
+
+    if (
+      // Check for all rows
+      cells.slice(0, 3).every((cell) => cell === player.token) ||
+      cells.slice(3, 6).every((cell) => cell === player.token) ||
+      cells.slice(6).every((cell) => cell === player.token) ||
+      // Check for all columns
+      [cells[0], cells[3], cells[6]].every((cell) => cell === player.token) ||
+      [cells[1], cells[4], cells[7]].every((cell) => cell === player.token) ||
+      [cells[2], cells[5], cells[8]].every((cell) => cell === player.token) ||
+      // Check for diagonals
+      [cells[0], cells[4], cells[8]].every((cell) => cell === player.token) ||
+      [cells[2], cells[4], cells[6]].every((cell) => cell === player.token)
+    ) {
+      console.log(player.id, "Won");
+      winner = player;
+    }
   };
 
   const playRound = (row, column) => {
+    if (winner) {
+      console.log("Game has ended.");
+      console.log("Winner: ", winner);
+      return;
+    }
     // Check if cell is available for move
     if (!gameBoard.cellIsAvailable(row, column)) {
       console.log("Cell is occupied");
       console.log(currentPlayer);
       return;
     }
-    gameBoard.drawMarker(row, column, currentPlayer);
+    gameBoard.dropMarker(row, column, currentPlayer.id);
     console.table(board);
     console.log(currentPlayer);
 
     // Check for winner
-    checkWinner();
+    checkWinner(currentPlayer);
+    if (winner) {
+      screenController.updateWinnerTitle(winner);
+    }
 
     // Only switch player if current player successfully made a move
     switchPlayer();
   };
 
-  return {playRound, switchPlayer};
+  return {playRound, getWinner, getCurrentPlayer};
 })();
+
+const screenController = (function () {
+  const winnerTitle = document.querySelector(".winner-title");
+  const gameGrid = document.querySelector(".game-grid");
+
+  gameGrid.addEventListener("click", (e) => {
+    const winner = gameController.getWinner();
+    if (winner) {
+      return;
+    }
+    const [row, column] = e.target.dataset.cell.split(",");
+    const currentPlayer = gameController.getCurrentPlayer();
+    gameController.playRound(row, column);
+    if (currentPlayer.id === "player1") {
+      e.target.textContent = "x";
+    } else {
+      e.target.textContent = "o";
+    }
+  });
+
+  const updateWinnerTitle = (winner) => {
+    winnerTitle.textContent = winner.id;
+  };
+
+  return {updateWinnerTitle};
+})(document);
 
 console.table(gameBoard.getBoard());
